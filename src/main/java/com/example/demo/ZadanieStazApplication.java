52659263bbe2dc54c0dcd1195d114dac9babd78d
DESKTOP-D84PV52\Fludu <fludaarek62@gmail.com>
2021-12-13 18:37:35 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/ZadanieStazApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZadanieStazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZadanieStazApplication.class, args);
+	}
+
+}
